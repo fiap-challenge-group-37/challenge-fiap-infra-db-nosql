1eaa7e4d3d3862147b9976ed9e3d216ef5733bc0
Renan Grande <renanlgrande@hotmail.com>
2026-01-08 19:34:31 -0300

    ajuste s3

--- data.tf
+++ data.tf
@@ -6,7 +6,7 @@
 data "terraform_remote_state" "k8s" {
   backend = "s3"
   config = {
-    bucket = "bucket-s3-g37-tc3"
+    bucket = "bucket-s3-g37-tc4"
     key    = "fiap/k8s/terraform.tfstate"
     region = "us-east-1"
   }
